badb61b3b58b3529ac63fe053206b73a77bc2b22
hugueitor <hbustam@gmail.com>
2023-06-11 19:59:50 -0400

    Adding env variables

--- record-sharing/apis/internal/src/init.js
+++ record-sharing/apis/internal/src/init.js
@@ -6,15 +6,15 @@ export var web3
 var contractData
 export var vars = {
     account_from : {
-        publicKey  : "0xC78c3BeA1e4fBBdAfBe5178A09EDD72ff2814a9d",
-        privateKey : "0x03d1ac8de34cfc4688bb6dcf3d5c1346e6e3c386dbb581303c323338006a34c8",
+        publicKey  : process.env.AURIUM_PUBLIC_KEY,
+        privateKey : process.env.AURIUM_PRIVATE_KEY,
     },
     besu_node : {
-        url        : "http://192.168.8.133:7545"
+        url        : process.env.AURIUM_NODE_URL
     },
     contractAbi    : "",
-    contractAccount: "0xC78c3BeA1e4fBBdAfBe5178A09EDD72ff2814a9d",
-    contractAddress: "0xD6BA555Add4f6E809ff3B558a4F2c6eF55e20a1E",
+    contractAccount: process.env.AURIUM_PUBLIC_KEY,
+    contractAddress: process.env.AURIUM_CONTRACT_ADDRESS,
     contractName   : "AclService",
 };
 
