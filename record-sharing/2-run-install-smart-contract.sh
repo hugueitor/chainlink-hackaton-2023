66e059767f70402b087d41aeb89ca8eb979a856a
hugueitor <hbustam@gmail.com>
2023-06-11 21:31:56 -0400

    Few changes

--- record-sharing/2-run-install-smart-contract.sh
+++ record-sharing/2-run-install-smart-contract.sh
@@ -7,5 +7,5 @@ echo "Compile..."
 truffle compile
 echo "Migrate..."
 truffle migrate --network rs
-echo "Test..."
-truffle test --network rs
+#echo "Test..."
+#truffle test --network rs
--- record-sharing/truffle-config.js
+++ record-sharing/truffle-config.js
@@ -1,10 +1,13 @@
 const PrivateKeyProvider = require("@truffle/hdwallet-provider");
 
 // Ganache
-const privateKey = process.env.AURIUM_PRIVATE_KEY
-const url  = process.env.AURIUM_NODE_URL
+const env_url    = process.env.AURIUM_NODE_URL
+const env_pk     = process.env.AURIUM_PRIVATE_KEY
+const url        = env_url
+const privateKey = env_pk.substr(2)
 
-console.log("truffle-config.js> Connecting to:", url)
+console.log("truffle-config.js> Connecting to: ", url)
+console.log("truffle-config.js> private key  : ", privateKey)
 
 module.exports = {
     networks: {
